e908a9ab34cceb78bd15bbe3e1add9f4a6e7f017
Chandler Owen <chantheman29@icloud.com>
2021-02-18 15:53:28 -0600

    commit on 'main'

---
+++ speed_template/speed/game/food.py
@@ -0,0 +1,25 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+# TODO: Define the Food class here
+#  Food class sets up where the food will appear
+class Food(Actor):
+
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+        self.set_text("@")
+        self.reset()
+
+    #  Check if the food was eaten to reset
+    def reset(self):
+        self._points = random.randint(1,5)
+        x = random.randint(1, constants.MAX_X - 2)
+        y = random.randint(1, constants.MAX_Y - 2)
+        position = Point(x, y)
+        self.set_position(position)
+
+    def get_points(self):
+        return self._points
